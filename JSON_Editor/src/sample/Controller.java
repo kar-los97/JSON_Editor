ed1b62cbc02c8100daf23f56aab479294d0e7afb
karlos-97 <KarlosIV+970918>
2020-09-10 20:21:01 +0200

    Zbyva poladit nacitani hodnot pole (JSValue != JSArrayValue) Zkouska nacteni treeView

--- JSON_Editor/src/sample/Controller.java
+++ JSON_Editor/src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
 public class Controller {
+    @FXML
+    TreeView<String> treeJS;
+
+    @FXML
+    private void initialize(){
+        TreeItem<String> rootItem = new TreeItem<String> ("Inbox");
+        rootItem.setExpanded(true);
+        for (int i = 1; i < 6; i++) {
+            TreeItem<String> item = new TreeItem<String> ("Message" + i);
+            rootItem.getChildren().add(item);
+        }
+        treeJS.setRoot(rootItem);
+    }
 }
